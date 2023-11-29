75714b211db871e2090b862e9d89bfde5ca31709
yfarah2005 <144721743+yfarah2005@users.noreply.github.com>
2023-11-29 21:52:46 +0000

    Assignment 4

--- script.js
+++ script.js
@@ -3,41 +3,130 @@
  * 
  *
  */
+let todoItems = [];
+let todoId = 0;
 
-
-//
-// Variables
-//
-
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
+// Load todo items from local storage when the page loads
+function loadTodoItems() {
+  const storedItems = JSON.parse(localStorage.getItem("todoItems"));
+  if (storedItems) {
+    todoItems = storedItems;
+    renderTodoList();
   }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
 }
 
-//
-// Inits & Event Listeners
-//
-inititialise();
+// Save todo items to local storage
+function saveTodoItems() {
+  localStorage.setItem("todoItems", JSON.stringify(todoItems));
+}
+
+function addToDoItem(text) {
+  if (typeof text !== "string") {
+    console.log("Please enter a string value");
+  } else {
+    let todoItem = {
+      id: todoId++,
+      text: text,
+      completed: false,
+    };
+    todoItems.push(todoItem);
+    renderTodoList();
+    saveTodoItems(); // Save to local storage after adding
+  }
+}
+
+function removeToDoItem(todoId) {
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id === todoId) {
+      todoItems.splice(i, 1);
+      renderTodoList();
+      saveTodoItems(); // Save to local storage after removing
+    }
+  }
+}
+
+function markToDoItemAsCompleted(todoId) {
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id === todoId) {
+      todoItems[i].completed = true;
+      renderTodoList();
+      saveTodoItems(); // Save to local storage after marking as completed
+    }
+  }
+}
+
+function deleteToDoItem(todoId) {
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id === todoId) {
+      todoItems.splice(i, 1);
+      renderTodoList();
+      saveTodoItems(); // Save to local storage after deleting
+    }
+  }
+}
+
+function clearCompletedTasks() {
+  for (let i = todoItems.length - 1; i >= 0; i--) {
+    if (todoItems[i].completed) {
+      todoItems.splice(i, 1);
+    }
+  }
+  renderTodoList();
+  saveTodoItems(); // Save to local storage after clearing completed tasks
+}
+
+function renderTodoList() {
+  const todoList = document.getElementById("todo-list");
+  todoList.innerHTML = "";
+  if (todoItems.length === 0) {
+    const emptyMessage = document.createElement("li");
+    emptyMessage.classList.add("empty-message");
+
+    const emptyText1 = document.createElement("p");
+    emptyText1.textContent = "No todos found.";
+    emptyMessage.appendChild(emptyText1);
+
+    const emptyText2 = document.createElement("p");
+    emptyText2.textContent = "🙈";
+    emptyMessage.appendChild(emptyText2);
+
+    todoList.appendChild(emptyMessage);
+  } else {
+    for (const todo of todoItems) {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `
+        <span class="${todo.completed ? "completed" : ""}">${todo.text}</span>
+        <button class="mark-as-done" data-id="${todo.id}">Mark as Done</button>
+        <button class="delete" data-id="${todo.id}">Delete</button>
+      `;
+      todoList.appendChild(listItem);
+    }
+  }
+ 
+}
+
+// Initial render when the page loads
+loadTodoItems();
+
+document.getElementById("todo-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const todoInput = document.getElementById("todo-input");
+  addToDoItem(todoInput.value);
+  todoInput.value = "";
+});
+
+document.getElementById("clear-todos").addEventListener("click", function () {
+  clearCompletedTasks();
+});
+
+document.getElementById("todo-list").addEventListener("click", function (event) {
+  const target = event.target;
+  if (target.tagName === "BUTTON") {
+    const todoId = parseInt(target.dataset.id);
+    if (target.classList.contains("mark-as-done")) {
+      markToDoItemAsCompleted(todoId);
+    } else if (target.classList.contains("delete")) {
+      deleteToDoItem(todoId);
+    }
+  }
+});
